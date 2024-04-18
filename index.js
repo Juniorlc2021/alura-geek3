7ad227de4dce3a92383627eaaa716b21687fc497
JUNIORLC <92834847+Juniorlc2021@users.noreply.github.com>
2024-04-18 18:00:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// pages/index.js
+
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Bem-vindo à Alura Geek!</h1>
+            <p>Esta é a página inicial do nosso site.</p>
+            {/* Adicione aqui o conteúdo que deseja exibir na página inicial */}
+        </div>
+    );
+};
+
+export default HomePage;
+
